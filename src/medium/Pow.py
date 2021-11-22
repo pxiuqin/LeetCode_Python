cd718cd194049b0f7936244eb947f6fc1eb9b72a
liangxiuqin <liangxiuqin@mininglamp.com>
2021-11-22 10:47:11 +0800

    update algo:Pow

--- src/medium/Pow.py
+++ src/medium/Pow.py
@@ -22,43 +22,109 @@
  * -10^4 <= x^n <= 10^4
  */
 """
+import ctypes
 
 
 class Pow:
-    """
-    * Divide-and-Conquer method
-    * For example:
-    * <p>
-    * 3^9=(3^4)^2*3
-    * ↓
-    * 3^4=(3^2)^2
-    * ↓
-    * 3^2=3*3
-    * ↓
-    * 3=3
-    * <p>
-    * So, both Space and Time are O(logN)
-    """
-
     def recursion(self, x, n):
+        """
+        * Divide-and-Conquer method
+        * For example:
+        * <p>
+        * 3^9=(3^4)^2*3
+        * ↓
+        * 3^4=(3^2)^2
+        * ↓
+        * 3^2=3*3
+        * ↓
+        * 3=3
+        * <p>
+        * So, both Space and Time are O(logN)
+        """
         if n == 1:
             return x
 
         # we`d better use unsigned right shift
-        half = self.recursion(x, n > 1)
+        half = self.recursion(x, Pow.unsigned_right_shitf(n, 1))
 
         if n & 1 == 0:
             return half * half
         else:
             return half * half * x
 
+    @staticmethod
+    def int_overflow(val):
+        maxint = 2147483647
+        if not -maxint - 1 <= val <= maxint:
+            val = (val + (maxint + 1)) % (2 * (maxint + 1)) - maxint - 1
+        return val
+
+    @staticmethod
+    def unsigned_right_shitf(n, i):
+        if n < 0:
+            n = ctypes.c_uint32(n).value
+
+        if i < 0:
+            return - Pow.int_overflow(n << abs(i))
+
+        return Pow.int_overflow(n >> i)
+
+    def pow1(self, x, n):
+        if n == 0 or x == 1:
+            return 1
+
+        result = self.recursion(x, abs(n))
+
+        if n > 0:
+            return result
+        else:
+            return 1 / result
+
+    def bitFunction(self, x, n):
+        """
+       * Solution with bit-manipulation
+       * For example:
+       * 9=1001
+       * 3^9=(3^1)^1*(3^2)^0*(3^4)^0*(3^8)^1
+       * Space is O(1), Time is O(logN)
+        """
+        multy = 1
+        base = x
+        i = n
+        while i >= 1:
+            if i & 1 > 0:
+                multy *= base
+
+            base *= base
+            i = Pow.unsigned_right_shitf(i, 1)
+
+        return multy
+
+    def pow2(self, x, n):
+        if n == 0 or x == 1:
+            return 1
+
+        # Avoid being out of bounds, we should cast into to long
+        m = n
+        result = self.bitFunction(x, abs(m))
+
+        if n > 0:
+            return result
+        else:
+            return 1 / result
+
 
 def main():
     obj = Pow()
-    print(obj.recursion(2, 10))
-    print(obj.recursion(2.1, 3))
-    print(obj.recursion(2, -2))
+    print(obj.pow1(2, 10))
+    print(obj.pow1(2.1, 3))
+    print(obj.pow1(2, -2))
+
+    print(obj.pow2(2, 10))
+    print(obj.pow2(2.1, 3))
+    print(obj.pow2(2, -2))
 
 
 if __name__ == '__main__':
+    # print(Pow.unsigned_right_shitf(-2,2))
     main()
